4c66cc3eef280a0dd364aa6c153d3048ef748f3d
Nabendu Bose <nabendubose1991@gmail.com>
2021-10-08 15:41:23 +0530

    adding routing path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",
+  component:LoginComponent
+  },
+  {path:"signup",
+  component:SignupComponent
+  },
+  {path:"profile",
+  component:ProfileComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
